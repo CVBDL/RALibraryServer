996987254e113504827246764e7d4afa42a5faa3
DeanYQ <casterd@sina.com>
2017-07-18 09:26:04 +0800

    deliver RA library email project

---
+++ RaLibrary.Email/EmailConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Linq;
+
+namespace RaLibrary.Email
+{
+    /// <summary>
+    /// Configuration of email.
+    /// </summary>
+    /// <remarks>
+    /// Provide SMTP server and email template.
+    /// </remarks>
+    public class EmailConfig
+    {
+        public string SenderName { get; set; }
+
+        public string SenderPassword { get; set; }
+
+        public string SmtpServer { get; set; }
+
+        public string SubjectTemplate { get; set; }
+
+        public string BodyTemplate { get; set; }
+
+        private EmailConfig()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            var doc = XDocument.Load("MailParam.xml");
+            SenderName = doc.Root.Element("senderName").Value;
+            SenderPassword = doc.Root.Element("senderPassword").Value;
+            SmtpServer = doc.Root.Element("smtpServer").Value;
+            SubjectTemplate = doc.Root.Element("subjectTemplate").Value;
+            BodyTemplate = doc.Root.Element("bodyTemplate").Value;
+        }
+
+        private static EmailConfig instance;
+        public static EmailConfig Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new EmailConfig();
+                return instance;
+            }
+        }
+    }
+}
---
+++ RaLibrary.Tests/RalibraryEmailTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaLibrary.Email;
+
+namespace RALibrary.Tests
+{
+    [TestClass]
+    public class RaLibraryEmailTest
+    {
+        [TestMethod]
+        public void RalibraryEmailTest_Send_Success()
+        {
+            var entity = new EmailEntity();
+            //entity.To.Add("test@***.com");
+            //entity.Cc.Add("test@***.com");
+            entity.Subject = EmailConfig.Instance.SubjectTemplate;
+            entity.Body = EmailConfig.Instance.BodyTemplate;
+
+            EmailAgent.Instance.Send(entity);
+        }
+    }
+}
