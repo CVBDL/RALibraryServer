48d358ee1c936822bd01837acec6cc94336eb3cf
HHDong1988 <donghuanhuan1988@hotmail.com>
2017-07-21 16:14:07 +0800

    add authendication moduel

---
+++ RAAuthentication/UserInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAAuthentication
+{
+    public class UserInfo
+    {
+        public UserInfo(string name, string email)
+        {
+            userName = name;
+            emailAddress = email;
+        }
+        public string userName { get; set; }
+        public string emailAddress { get; set; }
+    }
+}
