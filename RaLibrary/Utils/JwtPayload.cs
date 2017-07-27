df364b54a62dec4cde62ae26fbc9b6623c1c0e81
zhongsp <patrick.zhongsp@gmail.com>
2017-07-27 14:52:06 +0800

    update token payload

--- RaLibrary/Utils/JwtPayload.cs
+++ RaLibrary/Utils/JwtPayload.cs
@@ -2,7 +2,34 @@
 {
     public class JwtPayload
     {
+        /// <summary>
+        /// Token issuer.
+        /// </summary>
+        public string Iss { get; set; }
+
+        /// <summary>
+        /// Issued At.
+        /// </summary>
+        public long Iat { get; set; }
+
+        /// <summary>
+        /// Expiration Time.
+        /// </summary>
         public long Exp { get; set; }
+
+        /// <summary>
+        /// Audience. The username of domain account.
+        /// </summary>
+        public string Aud { get; set; }
+
+        /// <summary>
+        /// User email address read from Active Directory.
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// User name read from Active Directory.
+        /// </summary>
+        public string Name { get; set; }
     }
 }
