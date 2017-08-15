8ba929d6e616c66ac4a15ee3a8c6e49f0facc09a
HHDong1988 <donghuanhuan1988@hotmail.com>
2017-08-15 12:53:47 +0800

    add max length restriction

--- RaLibrary/Models/Book.cs
+++ RaLibrary/Models/Book.cs
@@ -7,11 +7,14 @@ namespace RaLibrary.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public string Code { get; set; }
 
         [Required]
+        [MaxLength(200)]
         public string Title { get; set; }
 
+        [MaxLength(50)]
         public string Borrower { get; set; }
     }
 }
